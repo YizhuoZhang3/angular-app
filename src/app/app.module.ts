bf08b040d838172748c6e43b096cf8677503c54b
Zoe Zhang <zhangyizhuozoe@gmail.com>
2022-03-09 11:30:33 -0500

    change color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { ServersComponent } from './servers/servers.component';
 @NgModule({
   declarations: [
     AppComponent, 
-    ServerComponent, ServersComponent
+    ServerComponent, 
+    ServersComponent
   ],
   imports: [
     BrowserModule,

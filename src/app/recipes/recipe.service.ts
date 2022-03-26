98fbc022ef4b7f55c31e18a44d9a9966869c3367
Zoe Zhang <zhangyizhuozoe@gmail.com>
2022-03-26 18:48:32 -0400

    modify recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,20 +6,21 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe', 
-      'This is a simple test', 
+      'Salmon Stir Fry', 
+      'Healthy Weeknight Meal', 
       'https://www.saveur.com/uploads/2020/11/20/Y7RZPFZEERAZVHJ2VHC2RXMEEY.jpg?quality=85&width=540',
       [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
+        new Ingredient('Salmon', 4),
+        new Ingredient('Green beans', 20)
       ]),
     new Recipe(
       'Another Recipe', 
       'This is a simple test', 
       'https://www.acouplecooks.com/wp-content/uploads/2020/12/Honey-Garlic-Shrimp-026.jpg', 
       [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
+        new Ingredient('Shrimp', 6),
+        new Ingredient('Broccoli', 10),
+        new Ingredient('Rice', 1)
       ])
   ];
 
